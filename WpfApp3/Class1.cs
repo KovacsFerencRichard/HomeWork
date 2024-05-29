4f50fada84ab8db3a6a3cd77049b028db3a3fc84
Kovács Richárd <kovacsferencrichard@ktch.hu>
2024-05-29 21:37:28 +0200

    Add project files.

---
+++ WpfApp3/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfÉlelmiszer
+    public class Storage : INotifyPropertyChanged
+{
+    private int id;
+    public int Id
+    {
+        get { return id; }
+        set { id = value; OnProperty(); }
+    }
+    private string name;
+    public string Name
+    {
+        get { return name; }
+        set { name = value; OnProperty(); }
+    }
+    private int number;
+    public int Number
+    {
+        get { return number; }
+        set { number = value; OnProperty(); }
+    }
+    public Storage(string name, int number)
+    {
+        Name = name;
+        Number = number;
+    }
+    public Storage() { }
+    public override string? ToString()
+    {
+        return Name != null ? $"{Name} : {Number} darab" : "";
+    }
+    public event PropertyChangedEventHandler? PropertyChanged;
+    private void OnProperty([CallerMemberName] string propertyName = null)
+    {
+        if (PropertyChanged != null)
+        {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
+}
